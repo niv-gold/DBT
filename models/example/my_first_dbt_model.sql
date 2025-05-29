eec64fbce2031120a453cfe026be02ff0d22cb63
Niv Goldberg <niv1.goldberg1@gmail.com>
2025-05-29 08:52:32 +0000

    add a new roe t first model

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -14,14 +14,15 @@ with source_data as (
     select 1 as id
     union all
     select null as id
-
+    union all
+    select 2 as id
 )
 
 select *
 from source_data
 
 /*
-    Uncomment the line below to remove records with null `id` values
+    Uncomment the line below to remove records with null `id` values ssss
 */
 
 -- where id is not null
