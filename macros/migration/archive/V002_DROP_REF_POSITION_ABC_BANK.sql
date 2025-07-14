5dc14d38d96f4ee6b70301d2ecd4aeae28b44a7f
niv2 goldberg2 <niv2.goldberg2@gmail.com>
2025-07-14 12:18:26 +0000

    finish chapter 14

--- macros/migration/archive/V002_DROP_REF_POSITION_ABC_BANK.sql
+++ macros/migration/archive/V002_DROP_REF_POSITION_ABC_BANK.sql
@@ -1,3 +1,3 @@
-{% macro drop_REF_POSITION_ABC_BANK() %}
+{% macro V002_DROP_REF_POSITION_ABC_BANK() %}
     DROP TABLE IF EXISTS {{target.database}}.{{target.schema}}_REFINED.REF_POSITION_ABC_BANK;
 {% endmacro %}
